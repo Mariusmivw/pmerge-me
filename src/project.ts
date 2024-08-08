65775578eed4dc88315df7ec8b03b0a060fb7f39
Mariusmivw <mvan-wij@student.codam.nl>
2024-08-08 12:46:29 +0200

    Remove plugin

--- src/project.ts
+++ src/project.ts
@@ -1,11 +1,10 @@
 import {makeProject} from '@motion-canvas/core';
 
 import example from './scenes/example?scene';
-import PMergeMe from './plugin';
 
 export default makeProject({
   scenes: [example],
   variables: {test: 2},
   experimentalFeatures: true,
-  plugins: [PMergeMe()],
+  plugins: [],
 });
